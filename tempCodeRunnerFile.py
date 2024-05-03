5181f1af3e2a0c4c9efb9e33d8cb745e0aecc0d5
Nishikantdalavi <143350739+Nishikantdalavi@users.noreply.github.com>
2024-05-03 19:05:33 +0530

    Add files via upload
    AgroMart is a comprehensive digital marketplace designed to facilitate the buying and selling of agricultural products. Built on the principles of transparency, efficiency, and accessibility, AgroMart provides a user-friendly platform for farmers, buyers, and sellers to connect and transact in the agricultural sector.

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+from flask import Flask,render_template,request,session,redirect,url_for,flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import login_user,logout_user,login_manager,LoginManager
+from flask_login import login_required,current_user
+
+
+# MY db connection
